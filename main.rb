410b37f5ff743747d1e0a0c3b383617e6bf7faf0
irenema <irene.ma@outlook.com>
2019-06-09 23:31:50 +1000

    Add ability to go prev and next

--- main.rb
+++ main.rb
@@ -4,20 +4,55 @@ require "pry"
 require "sinatra/reloader"
 require "httparty"
 
+enable  :sessions
+
 get '/' do
   erb :index
 end
 
 get '/tickets' do
   auth = {:username => "irene.ma@outlook.com", :password => "temp"}
-  response = HTTParty.get("https://irene.zendesk.com/api/v2/tickets.json?per_page=2", 
+  response = HTTParty.get("https://irene.zendesk.com/api/v2/tickets.json?per_page=25", 
                           :basic_auth => auth)
-                          
-  puts(response)
-  puts(response['tickets'])
- 
+
+  @tickets = response['tickets']
+
+  session[:next_page_url] = response['next_page']
+  session[:prev_page_url] = response['previous_page']
+
   erb :tickets
 end
+
+get '/tickets/next_page' do
+
+  next_page_url = session[:next_page_url]
+
+  auth = {:username => "irene.ma@outlook.com", :password => "temp"}
+  response = HTTParty.get(next_page_url, 
+                          :basic_auth => auth)
+
+  @tickets = response['tickets']
+  session[:next_page_url] = response['next_page']
+  session[:prev_page_url] = response['previous_page']
+                   
+  erb :tickets
+end
+
+get '/tickets/prev_page' do
+
+  prev_page_url = session[:prev_page_url]
+
+  auth = {:username => "irene.ma@outlook.com", :password => "temp"}
+  response = HTTParty.get(prev_page_url, 
+                          :basic_auth => auth)
+
+  @tickets = response['tickets']
+  session[:next_page_url] = response['next_page']
+  session[:prev_page_url] = response['previous_page']
+                   
+  erb :tickets
+end
+
 
 
 
